b92bdb8a4d2ef4d1f6c29d6a166549f9f4a53235
Vasilii Pusovoit <vasily.pustovoit@mail.utoronto.ca>
2024-03-01 14:19:25 -0500

    Added the pulling of notes

---
+++ git_pull_notes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Location of the notes
+notes_main_dir="$HOME/research/notes"
+
+# Cleanup the notes
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$SCRIPT_DIR"
+
+# Loop through all directories in the notes directory
+cd "$notes_main_dir" || { echo "Notes directory doesn't exist"; exit; }
+
+git pull
+echo ""
+echo "Pulled all the notes now"
