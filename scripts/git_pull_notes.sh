b5b976b11b45407d52865406f0ca381e1b89b546
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-03-13 15:52:15 -0400

    Added an option to automatically create git repo in case one hasn't when git pull

--- scripts/git_pull_notes.sh
+++ scripts/git_pull_notes.sh
@@ -2,14 +2,23 @@
 
 # Location of the notes
 notes_main_dir="$HOME/research/notes"
+git_notes="git@github.com:Vasissualiyp/Research_notes.git"
+
+create_notes_dir() {	
+        echo "Notes directory doesn't exist. Creating...";
+		mkdir -p "$notes_main_dir"
+		cd "$notes_main_dir"
+		git init
+		git remote add origin "$git_notes"
+		git pull origin main
+}
 
 # Cleanup the notes
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$SCRIPT_DIR"
 
 # Loop through all directories in the notes directory
-cd "$notes_main_dir" || { echo "Notes directory doesn't exist"; exit; }
+{ cd "$notes_main_dir"; git pull; } || create_notes_dir
 
-git pull
 echo ""
 echo "Pulled all the notes now"
