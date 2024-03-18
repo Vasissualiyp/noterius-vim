4d91badc50083b37f679f9401b3c7f2e9717b2a7
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2024-03-18 09:29:26 -0400

    Removed automatic hotkey generation

--- lua/noterius-vim/noterius_telescope.lua
+++ lua/noterius-vim/noterius_telescope.lua
@@ -5,16 +5,6 @@ M.notes_dir = vim.fn.stdpath('data') .. '/noterius_vim/notes' -- Default path
 function M.setup(opts)
   opts = opts or {}
   M.notes_dir = opts.notes_dir or M.notes_dir
-
-  local keymaps = opts.keymaps or {
-    ["<leader>nf"] = "search_notes",
-    ["<leader>ng"] = "grep_notes",
-  }
-
-  for k, v in pairs(keymaps) do
-    local cmd = string.format(":lua require('noterius_vim.noterius_telescope').%s()<CR>", v)
-    vim.api.nvim_set_keymap('n', k, cmd, {noremap = true, silent = true})
-  end
 end
 
 function M.search_notes()
