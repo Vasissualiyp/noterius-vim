8830403822c80cc8835b852f8d1cfb6caa532f27
Vasilii Pustovoit <vasily.pustovoit@mail.utoronto.ca>
2024-03-18 14:38:13 -0400

    Re-pasted the lua funcitons

--- lua/noterius-vim/noterius_telescope.lua
+++ lua/noterius-vim/noterius_telescope.lua
@@ -7,21 +7,31 @@ function M.setup(opts)
   M.notes_dir = opts.notes_dir or M.notes_dir
 end
 
-function M.search_notes()
-  require('telescope.builtin').find_files({
-    search_dirs = {M.notes_dir},
-    find_command = {'rg', '--files', '--type', 'tex', '--glob', '*.tex'},
-    path_display = {"tail"}
-  })
-end
-
+-- Function for live grep in notes
 function M.grep_notes()
   require('telescope.builtin').live_grep({
-    search_dirs = {M.notes_dir},
+    search_dirs = {notes_dir},
     additional_args = function(opts)
       return {"--glob", "*.tex"}
     end,
-    path_display = {"tail"}
+    path_display = function(opts, path)
+      local tail = require("telescope.utils").path_tail(path)
+      local relative_path = path:sub(#notes_dir + 2) -- +2 to remove the leading slash and make it relative
+      return string.gsub(relative_path, "(.*/)(%d+/%d+/%d+)/(.*)", "%2/" .. tail)
+    end,
+  })
+end
+
+-- Function for finding files in notes
+function M.find_notes()
+  require('telescope.builtin').find_files({
+    search_dirs = {notes_dir},
+    find_command = {'rg', '--files', '--type', 'tex', '--glob', '*.tex'},
+    path_display = function(opts, path)
+      local tail = require("telescope.utils").path_tail(path)
+      local relative_path = path:sub(#notes_dir + 2)
+      return string.gsub(relative_path, "(.*/)(%d+/%d+/%d+)/(.*)", "%2/" .. tail)
+    end,
   })
 end
 
