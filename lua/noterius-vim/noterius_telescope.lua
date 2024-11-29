a82e026c5d01f9b26f1abadabf9db7b7c519913d
Vasissualiyp <vpustovoit@cita.utoronto.ca>
2024-11-29 10:52:08 -0500

    More error handling, fixing telescope...

--- lua/noterius-vim/noterius_telescope.lua
+++ lua/noterius-vim/noterius_telescope.lua
@@ -1,12 +1,11 @@
 local M = {}
 local default_notes_path = vim.fn.expand('~/research/notes')
 
--- Initialize notes_dir with a default path
-M.notes_dir = vim.fn.expand(vim.g.noterius_notes_dir or default_notes_path)
+M.notes_dir = vim.g.noterius_notes_dir or default_notes_path
 
 function M.setup(opts)
   opts = opts or {}
-  M.notes_dir = vim.fn.expand(opts.notes_dir or M.notes_dir)
+  M.notes_dir = opts.notes_dir or M.notes_dir
 end
 
 -- Function for live grep in notes
@@ -15,12 +14,14 @@ function M.grep_notes()
     vim.notify("Notes directory does not exist: " .. M.notes_dir, vim.log.levels.ERROR)
     return
   end
+
   require('telescope.builtin').live_grep({
     search_dirs = {M.notes_dir},
     additional_args = function(opts)
       return {"--glob", "*.tex"}
     end,
     path_display = function(opts, path)
+      if not path then return '' end
       local tail = require("telescope.utils").path_tail(path)
       local relative_path = path:sub(#M.notes_dir + 2)
       return string.gsub(relative_path, "(.*/)(%d+/%d+/%d+)/(.*)", "%2/" .. tail)
@@ -30,10 +31,16 @@ end
 
 -- Function for finding files in notes
 function M.search_notes()
+  if vim.fn.isdirectory(M.notes_dir) == 0 then
+    vim.notify("Notes directory does not exist: " .. M.notes_dir, vim.log.levels.ERROR)
+    return
+  end
+
   require('telescope.builtin').find_files({
     search_dirs = {M.notes_dir},
     find_command = {'rg', '--files', '--type', 'tex', '--glob', '*.tex'},
     path_display = function(opts, path)
+      if not path then return '' end
       local tail = require("telescope.utils").path_tail(path)
       local relative_path = path:sub(#M.notes_dir + 2)
       return string.gsub(relative_path, "(.*/)(%d+/%d+/%d+)/(.*)", "%2/" .. tail)
